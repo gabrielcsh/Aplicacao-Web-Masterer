0d648f6cd62c806c71b28049f109760b7159e1b8
Gabriel Cardoso dos Santos <47262913+gabrielcsh@users.noreply.github.com>
2022-05-22 09:37:19 -0400

    Add files via upload
    Criando os componentes com PHP para reutilizar também o HTML em várias páginas

---
+++ views/Header/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap" rel="stylesheet" />
+    <title>Masterer</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class="header-side">
+            <div class="header-buttons">
+                <a href="" target="_blank" rel="alternate">
+                    <img src="../../icons/person-icon.png" alt="Usuario" width="25px" height="25px">
+                </a>
+                <a href="" target="_blank" rel="alternate">
+                    <img src="../../icons/fone-icon.png" alt="Contato" width="25px" height="25px">
+                </a>
+                <a href="" target="_blank" rel="alternate">
+                    <img src="../../icons/sale-icon.png" alt="Carrinho" width="25px" height="25px">
+                </a>
+            </div>
+            <div class="header-center">
+                <a href="" target="_self" rel="alternate">
+                    <img src="../../icons/masterer-icon.png" alt="Masterer">
+                </a>
+            </div>
+            <div class="header-search">
+                <input type="text" id="search-product" name="search-product">
+            </div>
+        </div>
+        <div class="header-categories">
+            <a href="http://" id="homepage" target="_blank" rel="alternate">HOME</a>
+            <a href="http://" id="colecoes" target="_blank" rel="alternate">COLEÇÕES</a>
+            <a href="http://" id="camisetas" target="_blank" rel="alternate">CAMISETAS</a>
+            <a href="http://" id="regatas" target="_blank" rel="alternate">REGATAS</a>
+            <a href="http://" id="moletons" target="_blank" rel="alternate">MOLETONS</a>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ views/Home/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="../Header/header.css" />
+  <link rel="stylesheet" href="./index.css" />
+  <link rel="stylesheet" href="../Footer/footer.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap" rel="stylesheet" />
+  <title>Masterer</title>
+</head>
+
+<body>
+  <?php include "../Header/header.php" ?>
+  <div class="content">
+    <div class="banner"></div>
+  </div>
+  <?php include "../Footer/footer.php" ?>
+
+  </div>
+</body>
+
+</html>
